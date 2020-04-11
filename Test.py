5d6381875914b9ad2c6367a6957afb4855bffaef
HanyueZheng <zhenghanyue813@gmail.com>
2020-04-11 16:21:50 -0500

    test

--- Test.py
+++ Test.py
@@ -106,7 +106,7 @@ num_samples = len(test)
 num_batches = int(num_samples/param.batch_size)
 
 
-epoch = 10
+epoch = 40
 
 encoder = torch.load(f='model/encoder_%s_%s.pckl' % ("copynet",str(epoch)))
 decoder = torch.load(f='model/decoder_%s_%s.pckl' % ("copynet",str(epoch)))
